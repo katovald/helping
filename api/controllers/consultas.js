57daa2142b53a87661f53392b504e22a8aa9435e
kato <kvaldez@centraldealarmas.com.mx>
2021-02-24 11:57:51 -0600

    lista respuesta envio, solo falta el querie

--- api/controllers/consultas.js
+++ api/controllers/consultas.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const knex = require('knex')({
-    client: 'mssql',
-    connection: {
-        server: 'sql-2008',
-        user: 'gsiconnectusr',
-        password: 'Absolut2',
-        options: {
-            port: 1433,
-            database: 'gsiconnectdb',
-            encrypt: false
-        }
-    }
-});
-
 module.exports = {
     getQuerie : getQuerie   //    GET     /servicio/{tipo}
 }
